d4bbc35b2e51f9e7108cb5c3ea4bb317b73d36ec
selim-IDBCI <selim.saied@univ-rennes.fr>
2025-11-19 13:04:59 +0100

    tests terrain

--- test/test_terrain.py
+++ test/test_terrain.py
@@ -1,4 +1,3 @@
-
 import unittest
 import xmlrunner
 
@@ -7,14 +6,28 @@ from Terrain import Terrain, Case
 class TestTerrain(unittest.TestCase):
 
     def test_chargement(self):
-        # TODO
-        self.fail()
+        t = Terrain("../terrains/t1.txt")
+
+        # Vérifier que l’entrée est bien trouvée
+        self.assertEqual(t.source, (0, 0))
+
+        # Vérifier que les clients sont bien trouvés
+        self.assertIn((1, 0), t.clients)
+        self.assertIn((1, 2), t.clients)
+        self.assertIn((2, 1), t.clients)
+
+        # Vérifier qu’un obstacle est bien trouvé
+        self.assertIn((1, 1), t.obstacles)
+
+        # Vérifier que la grille est bien remplie
+        self.assertEqual(t.grid[0][0], "E")
+        self.assertEqual(t.grid[1][1], "X")
 
     def test_accesseur(self):
-        t = Terrain()
+        t = Terrain.__new__(Terrain)  # créer un objet sans appeler __init__
         t.cases = [
-                [Case.ENTREE, Case.VIDE, Case.VIDE],
-                [Case.CLIENT, Case.CLIENT, Case.CLIENT],
+            [Case.ENTREE, Case.VIDE, Case.VIDE],
+            [Case.CLIENT, Case.CLIENT, Case.CLIENT],
         ]
         self.assertEqual(t[0][0], Case.ENTREE)
         self.assertEqual(t[0][1], Case.VIDE)
@@ -22,4 +35,3 @@ class TestTerrain(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output="test-reports"))
-
